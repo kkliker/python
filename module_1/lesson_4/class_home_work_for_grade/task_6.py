72413760f7534fd62ed55efd6b203b6d8f9e6628
kkliker <Sheeepoff@gmail.com>
2023-02-18 14:08:40 +0300

    lesson_5

---
+++ module_1/lesson_4/class_home_work_for_grade/task_6.py
@@ -0,0 +1,10 @@
+"""
+С клавиатуры вводятся 4 целых числа a, b, c и d. Выведите на экран результaт выражения a*b + c*d.
+
+"""
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(a * b + c * d)
